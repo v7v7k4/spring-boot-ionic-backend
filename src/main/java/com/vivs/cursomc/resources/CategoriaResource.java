0bd37955194d4ec4507ecbee38d9cdd3d6958522
Vivian Souza <viviantsd@gmail.com>
2019-08-31 16:16:19 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/vivs/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/vivs/cursomc/resources/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -62,6 +64,18 @@ public class CategoriaResource {
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> listaCategoria = categoriaService.findAll();
 		List<CategoriaDTO> listaCategoriaDTO = listaCategoria.stream().map(dto -> new CategoriaDTO(dto)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listaCategoriaDTO);
+	}
+	
+	//localhost:8080/categorias/page?page=0&linesPerPage=5
+	@GetMapping(value="/page")
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Categoria> listaCategoria = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listaCategoriaDTO = listaCategoria.map(dto -> new CategoriaDTO(dto));
 		return ResponseEntity.ok().body(listaCategoriaDTO);
 	}
 }
