ebb2d926751238445b9b907cdeb9beb85e7c7cee
Vivian Souza <viviantsd@gmail.com>
2019-09-17 22:31:39 -0300

    AuthenticationFailureHandler para adicionar o 401 não autorizado

--- src/main/java/com/vivs/cursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/com/vivs/cursomc/security/JWTAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.vivs.cursomc.security;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -12,27 +13,30 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vivs.cursomc.dto.CredenciaisDTO;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-	
+
 	/*
 	 * Injeção de dependencia com o construtor
 	 */
 	private AuthenticationManager authenticationManager;
 
-    private JWTUtil jwtUtil;
-    /*
-     * /login é o endpoint padrão do springsecurity para autenticação http://localhost:8080/login
-     */
-    public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
-    	//setAuthenticationFailureHandler(new JWTAuthenticationFailureHandler());
-        this.authenticationManager = authenticationManager;
-        this.jwtUtil = jwtUtil;
-    }
+	private JWTUtil jwtUtil;
+
+	/*
+	 * /login é o endpoint padrão do springsecurity para autenticação
+	 * http://localhost:8080/login
+	 */
+	public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+		setAuthenticationFailureHandler(new JWTAuthenticationFailureHandler());
+		this.authenticationManager = authenticationManager;
+		this.jwtUtil = jwtUtil;
+	}
 
 	@Override
 	public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res)
@@ -54,10 +58,27 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	@Override
 	protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
 			Authentication auth) throws IOException, ServletException {
-		//gerar um token e acrescentar na resposta da requisição
+		// gerar um token e acrescentar na resposta da requisição
 
 		String username = ((UserSS) auth.getPrincipal()).getUsername();
 		String token = jwtUtil.generateToken(username);
 		res.addHeader("Authorization", "Bearer " + token);
+	}
+
+	private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+		@Override
+		public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+				AuthenticationException exception) throws IOException, ServletException {
+			response.setStatus(401);
+			response.setContentType("application/json");
+			response.getWriter().append(json());
+		}
+
+		private String json() {
+			long date = new Date().getTime();
+			return "{\"timestamp\": " + date + ", " + "\"status\": 401, " + "\"error\": \"Não autorizado\", "
+					+ "\"message\": \"Email ou senha inválidos\", " + "\"path\": \"/login\"}";
+		}
 	}
 }
