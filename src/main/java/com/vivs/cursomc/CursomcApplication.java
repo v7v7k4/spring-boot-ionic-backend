ae2a005806a0c161fd9a9d4b818159a555bbac7a
Vivian Souza <viviantsd@gmail.com>
2019-09-22 17:19:40 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/vivs/cursomc/CursomcApplication.java
+++ src/main/java/com/vivs/cursomc/CursomcApplication.java
@@ -1,11 +1,17 @@
 package com.vivs.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.vivs.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3service;
 	
 	
 	public static void main(String[] args) {
@@ -15,6 +21,7 @@ public class CursomcApplication implements CommandLineRunner {
 	//executar a instanciação no momento que a aplicação iniciar
 	@Override
 	public void run(String... args) throws Exception {
-			}
+		s3service.upload("C:\\teste\\IMG-20161225-WA0029.jpg");
+	}
 
 }
