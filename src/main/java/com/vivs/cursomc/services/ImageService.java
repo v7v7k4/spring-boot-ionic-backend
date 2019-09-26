cfff5e89a06fce483718d016bd8bc20d3348080d
Vivian Souza <viviantsd@gmail.com>
2019-09-25 21:54:16 -0300

    Ajustando tamanho da imagem

--- src/main/java/com/vivs/cursomc/services/ImageService.java
+++ src/main/java/com/vivs/cursomc/services/ImageService.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,14 +20,14 @@ import com.vivs.cursomc.services.exceptions.FileException;
 public class ImageService {
 	public BufferedImage getJpgImageFromFile(MultipartFile uploadedFile) {
 		String ext = FilenameUtils.getExtension(uploadedFile.getOriginalFilename());
-		if(!"png".equals(ext) && !"jpg".equals(ext)) {
+		if (!"png".equals(ext) && !"jpg".equals(ext)) {
 			throw new FileException("Somente imagens PNG e JPG são permitidas");
 		}
-		
+
 		try {
 			BufferedImage img = ImageIO.read(uploadedFile.getInputStream());
-			//se for png precisa transformar em jpg
-			if("png".equals(ext)) {
+			// se for png precisa transformar em jpg
+			if ("png".equals(ext)) {
 				img = pngToJpg(img);
 			}
 			return img;
@@ -40,7 +41,7 @@ public class ImageService {
 		jpgImage.createGraphics().drawImage(img, 0, 0, Color.WHITE, null);
 		return jpgImage;
 	}
-	
+
 	public InputStream getInputStream(BufferedImage img, String extension) {
 		try {
 			ByteArrayOutputStream os = new ByteArrayOutputStream();
@@ -49,6 +50,16 @@ public class ImageService {
 		} catch (IOException e) {
 			throw new FileException("Erro ao ler arquivo");
 		}
+	}
+
+	public BufferedImage cropSquare(BufferedImage sourceImg) {
+		int min = (sourceImg.getHeight() <= sourceImg.getWidth()) ? sourceImg.getHeight() : sourceImg.getWidth();
+		return Scalr.crop(sourceImg, (sourceImg.getWidth() / 2) - (min / 2), (sourceImg.getHeight() / 2) - (min / 2),
+				min, min);
+	}
+	
+	public BufferedImage resize(BufferedImage sourceImg, int size) {
+		return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
 	}
 
 }
