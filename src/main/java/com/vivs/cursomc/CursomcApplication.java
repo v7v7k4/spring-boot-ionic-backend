51bbf844738238f53aa5785b831fc3d3e0173e3b
Vivian Souza <viviantsd@gmail.com>
2019-09-29 20:34:54 -0300

    Aumentando o tamanho máximo permitido para upload

--- src/main/java/com/vivs/cursomc/CursomcApplication.java
+++ src/main/java/com/vivs/cursomc/CursomcApplication.java
@@ -1,11 +1,15 @@
 package com.vivs.cursomc;
 
+import javax.servlet.MultipartConfigElement;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.util.unit.DataSize;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -28,7 +32,7 @@ public class CursomcApplication implements CommandLineRunner {
 		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
 		messageSource.setBasename("classpath:messages");
 		messageSource.setDefaultEncoding("UTF-8");
-		//messageSource.setUseCodeAsDefaultMessage(true);
+		// messageSource.setUseCodeAsDefaultMessage(true);
 		return messageSource;
 	}
 
@@ -38,5 +42,13 @@ public class CursomcApplication implements CommandLineRunner {
 //		bean.setValidationMessageSource(messageSource());
 //		return bean;
 //	}
+
+	@Bean
+	MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setMaxFileSize(DataSize.ofBytes(512000000L));
+		factory.setMaxRequestSize(DataSize.ofBytes(512000000L));
+		return factory.createMultipartConfig();
+	}
 
 }
